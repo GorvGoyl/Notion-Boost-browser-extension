3c62b812e8be5b937030ab50368a41cff99cdcd7
Gourav <1gouravgg@gmail.com>
2021-09-14 02:58:22 +0530

    new linkcomponent icon

--- src/js/feature/rollupUrlClickable.js
+++ src/js/feature/rollupUrlClickable.js
@@ -78,6 +78,36 @@ function getUrl(textParam) {
 let linkComponentEl = "";
 let rollupCellEl = "";
 
+function createLinkComponent(url) {
+  return toElement(
+    `<div class="linkComponent">
+    <div>
+      <a
+        title="Open URL in new tab"
+        href="${url}"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="notion-focusable"
+      >
+            <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+        />
+        <path
+          d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+        />
+      </svg>
+      </a>
+    </div>
+  </div>
+  `
+  );
+}
+
 function handleTableHover(e) {
   // console.log("hover over table", e.target);
   let nestedLevel = 0;
@@ -142,32 +172,7 @@ function handleTableHover(e) {
     if (url) {
       rollupCellEl.style.position = "relative";
 
-      linkComponentEl = toElement(
-        `<div class="linkComponent">
-        <div>
-          <a
-            title="Open URL in new tab"
-            href="${url}"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="notion-focusable"
-          >
-          <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-            clip-rule="evenodd"
-          />
-        </svg>
-          </a>
-        </div>
-      </div>
-      `
-      );
+      linkComponentEl = createLinkComponent(url);
 
       rollupCellEl.appendChild(linkComponentEl);
 
@@ -244,32 +249,7 @@ function handletableRowAsPageHover(e) {
     if (url) {
       rollupCellEl.style.position = "relative";
 
-      linkComponentEl = toElement(
-        `<div class="linkComponent">
-        <div>
-          <a
-            title="Open URL in new tab"
-            href="${url}"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="notion-focusable"
-          >
-          <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-            clip-rule="evenodd"
-          />
-        </svg>
-          </a>
-        </div>
-      </div>
-      `
-      );
+      linkComponentEl = createLinkComponent(url);
 
       rollupCellEl.appendChild(linkComponentEl);
     }
